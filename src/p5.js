ce47681a2ec5f7cd4f70d42437472832582379db
IvanT69 <ivanthy2005@gmail.com>
2025-07-02 23:30:28 +0800

    Update p5.js

--- src/p5.js
+++ src/p5.js
@@ -1,1 +1,10 @@
-
+function p5_symmetricDiff(list1, list2) {
+    let result = [];
+    for (let x of list1) {
+        if (!list2.includes(x) && !result.includes(x)) result.push(x);
+    }
+    for (let x of list2) {
+        if (!list1.includes(x) && !result.includes(x)) result.push(x);
+    }
+    return result;
+}
