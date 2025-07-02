238a0b231f95d9a4715d091beed313d06717c925
IvanT69 <ivanthy2005@gmail.com>
2025-07-02 23:27:39 +0800

    Update p1.js

--- src/p1.js
+++ src/p1.js
@@ -1,1 +1,10 @@
-
+function p1_sortNumbers(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIdx = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIdx]) minIdx = j;
+        }
+        [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+    }
+    return arr;
+}
