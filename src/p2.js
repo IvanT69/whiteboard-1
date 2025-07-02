9f74e8795ee410dba486fdb2f5470c4a63a43b1c
IvanT69 <ivanthy2005@gmail.com>
2025-07-02 23:29:43 +0800

    Update p2.js

--- src/p2.js
+++ src/p2.js
@@ -1,1 +1,11 @@
+function p2_fizzBuzz() {
+    let result = [];
+    for (let i = 1; i <= 100; i++) {
+        if (i % 15 === 0) result.push('FizzBuzz');
+        else if (i % 3 === 0) result.push('Fizz');
+        else if (i % 5 === 0) result.push('Buzz');
+        else result.push(i);
+    }
+    return result;
+}
 
