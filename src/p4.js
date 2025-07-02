889b9027eb0a3ea6d77afb6c2f92a922bf2e81b2
IvanT69 <ivanthy2005@gmail.com>
2025-07-02 23:30:11 +0800

    Update p4.js

--- src/p4.js
+++ src/p4.js
@@ -1,1 +1,11 @@
-
+function p4_intersection(list1, list2) {
+    let result = [];
+    for (let i of list1) {
+        for (let j of list2) {
+            if (i === j && !result.includes(i)) {
+                result.push(i);
+            }
+        }
+    }
+    return result;
+}
