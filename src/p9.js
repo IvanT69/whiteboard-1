8d91801aed42acdac8218387e4df02178bfe65cc
IvanT69 <ivanthy2005@gmail.com>
2025-07-02 23:31:33 +0800

    Update p9.js

--- src/p9.js
+++ src/p9.js
@@ -1,1 +1,12 @@
-
+function p9_findPath(graph, start, end, path = []) {
+    path.push(start);
+    if (start === end) return { exists: true, path };
+    if (!graph[start]) return { exists: false, path: [] };
+    for (let node of graph[start]) {
+        if (!path.includes(node)) {
+            const result = p9_findPath(graph, node, end, [...path]);
+            if (result.exists) return result;
+        }
+    }
+    return { exists: false, path: [] };
+}
