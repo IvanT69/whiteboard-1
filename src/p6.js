2e21fc4dba3d0f95149d4874e0008204fa1f9f43
IvanT69 <ivanthy2005@gmail.com>
2025-07-02 23:30:43 +0800

    Update p6.js

--- src/p6.js
+++ src/p6.js
@@ -1,1 +1,16 @@
-
+function p6_maxChar(str) {
+    const counts = {};
+    for (let char of str) {
+        if (char.match(/[\w\d]/)) {
+            counts[char] = (counts[char] || 0) + 1;
+        }
+    }
+    let maxChar = '', maxCount = 0;
+    for (let char in counts) {
+        if (counts[char] > maxCount) {
+            maxChar = char;
+            maxCount = counts[char];
+        }
+    }
+    return { character: maxChar, count: maxCount };
+}
