16394d90d9f6deef327a62e6c6467d417c71296f
IvanT69 <ivanthy2005@gmail.com>
2025-07-02 23:31:12 +0800

    Update p8.js

--- src/p8.js
+++ src/p8.js
@@ -1,1 +1,5 @@
+function p8_isAnagram(str1, str2) {
+    const normalize = s => s.toLowerCase().replace(/[^a-z0-9]/g, '').split('').sort().join('');
+    return normalize(str1) === normalize(str2);
+}
 
