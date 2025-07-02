7002fbd9d36355ae6dcde5733e9dc9b4fd125716
IvanT69 <ivanthy2005@gmail.com>
2025-07-02 23:29:57 +0800

    Update p3.js

--- src/p3.js
+++ src/p3.js
@@ -1,1 +1,8 @@
-
+function p3_fibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+    const seq = p3_fibonacci(n - 1);
+    seq.push(seq[seq.length - 1] + seq[seq.length - 2]);
+    return seq;
+}
